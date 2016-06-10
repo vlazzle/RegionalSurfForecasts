57b8090a85e7aa3fb3bc78be89c62273d93164d5
Vladimir Chernis <iamvlad@gmail.com>
2016-06-10 13:15:17 -0700

    updated steps for running locally without php

--- start.sh
+++ start.sh
@@ -3,4 +3,5 @@ cd /Library/WebServer/Documents
 sudo ln -s ~/surfspots/ .
 sudo apachectl start
 open http://localhost/surfspots
+# or http://localhost/surfspots/home.html if you don't have php enabled
 tail -f /var/log/apache2/error_log f /var/log/apache2/access_log
