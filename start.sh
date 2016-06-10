9c018c659017d6e219fa15235e3c00c4ccd000a0
Vladimir Chernis <iamvlad@gmail.com>
2016-06-10 13:19:45 -0700

    clean up start.sh script

--- start.sh
+++ start.sh
@@ -1,7 +1,11 @@
 #!/bin/bash
-cd /Library/WebServer/Documents
-sudo ln -s ~/surfspots/ .
+
+# This is for Mac OS X only.
+
+sudo ln -s `pwd` /Library/WebServer/Documents/surfspots
 sudo apachectl start
 open http://localhost/surfspots
 # or http://localhost/surfspots/home.html if you don't have php enabled
+
+# optionally, to watch logs:
 tail -f /var/log/apache2/error_log f /var/log/apache2/access_log
