ec765365206492a3ee981206ab4cd2db2a27d454
vlad <iamvlad@gmail.com>
2016-06-24 17:02:59 -0700

    updated start.sh to use heroku and npm

--- start.sh
+++ start.sh
@@ -1,11 +1,2 @@
 #!/bin/bash
-
-# This is for Mac OS X only.
-
-sudo ln -s `pwd` /Library/WebServer/Documents/surfspots
-sudo apachectl start
-open http://localhost/surfspots
-# or http://localhost/surfspots/home.html if you don't have php enabled
-
-# optionally, to watch logs:
-tail -f /var/log/apache2/error_log f /var/log/apache2/access_log
+heroku local web
